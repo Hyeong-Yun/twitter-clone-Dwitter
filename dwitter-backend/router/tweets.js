cf1d67665c8d3fc252e2405b00039fc3d195ac63
Hyeong-Yun <guddbs5545@inu.ac.kr>
2022-02-28 12:54:06 +0900

    2th: validation설정

--- dwitter-backend/router/tweets.js
+++ dwitter-backend/router/tweets.js
@@ -1,17 +1,30 @@
 import express from 'express';
 import 'express-async-errors';
 import * as tweeetController from '../controller/tweet.js';
+import {body} from 'express-validator'
+import {validate} from '../middleware/validator.js';
+
 const router = express.Router();
+
+// contract Testing: Clinet-server
+//
+const validateTweet =  [
+  body('text')
+  .trim()
+  .isLength({min: 3})
+  .withMessage('text should be at least 3 characters'), 
+  validate
+]
 
 router.get('/',tweeetController.getTweets)
 
 router.get('/:id', tweeetController.getTweet)
 
 //Post /tweets
-router.post('/', tweeetController.createTweet)
+router.post('/', validateTweet, tweeetController.createTweet)
 
 // PUT/tweets/:id
-router.put('/:id', tweeetController.updateTweet)
+router.put('/:id',validateTweet, tweeetController.updateTweet)
 
 // Delete/ tweets/:id
 router.delete('/:id', tweeetController.deleteTweet)
