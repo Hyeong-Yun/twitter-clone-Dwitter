3d49c592445ab3dc2ebe6541d1f17cbe0682ea8b
Hyeong-Yun <guddbs5545@inu.ac.kr>
2022-03-14 21:22:14 +0900

    Revert "2th: validation설정"
    This reverts commit cf1d67665c8d3fc252e2405b00039fc3d195ac63.

--- dwitter-backend/router/tweets.js
+++ dwitter-backend/router/tweets.js
@@ -1,30 +1,17 @@
 import express from 'express';
 import 'express-async-errors';
 import * as tweeetController from '../controller/tweet.js';
-import {body} from 'express-validator'
-import {validate} from '../middleware/validator.js';
-
 const router = express.Router();
-
-// contract Testing: Clinet-server
-//
-const validateTweet =  [
-  body('text')
-  .trim()
-  .isLength({min: 3})
-  .withMessage('text should be at least 3 characters'), 
-  validate
-]
 
 router.get('/',tweeetController.getTweets)
 
 router.get('/:id', tweeetController.getTweet)
 
 //Post /tweets
-router.post('/', validateTweet, tweeetController.createTweet)
+router.post('/', tweeetController.createTweet)
 
 // PUT/tweets/:id
-router.put('/:id',validateTweet, tweeetController.updateTweet)
+router.put('/:id', tweeetController.updateTweet)
 
 // Delete/ tweets/:id
 router.delete('/:id', tweeetController.deleteTweet)
